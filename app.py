7472895c430651c28a36fd123fabb5bb10dfe99f
michelle 3 <142525477+michelleprabhu@users.noreply.github.com>
2025-02-06 12:44:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import json
 import os
 import time
 import asyncio
-from langchain_community.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
+
 
 
 def validate_nct_id(nct_id):
